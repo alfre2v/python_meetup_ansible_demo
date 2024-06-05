c0e485b797626b3bb6b5a370a6a5a50e0a9037c6
Alfredo Valles <alfredo.valles@gmail.com>
2024-06-05 02:19:45 -0500

    Add polls app logic

--- tutor_site/polls/tests.py
+++ tutor_site/polls/tests.py
@@ -1,3 +1,37 @@
+import datetime
+
+from django.utils import timezone
+
 from django.test import TestCase
 
+from .models import Question
+
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_date(self):
+        """
+        check was_published_recently() returns False for questions whose pub_date is in the future
+        """
+        future_time = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(pub_date=future_time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        check was_published_recently() returns False for questions whose pub_date is more than 1 day old
+        """
+        old_time = timezone.now() - datetime.timedelta(days=2)
+        old_question = Question(pub_date=old_time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_new_question(self):
+        """
+        check was_published_recently() returns True for questions whose pub_date is less than 1 day old
+        """
+        recent_time = timezone.now() - datetime.timedelta(days=0.5)
+        recent_question = Question(pub_date=recent_time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
--- tutor_site/polls/views.py
+++ tutor_site/polls/views.py
@@ -1,10 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
 
-from django.http import HttpRequest, HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index(request: HttpRequest) -> HttpResponse:
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_questions_list': latest_questions_list,
+    }
+    return render(request, 'polls/index.html', context=context)
 
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    context = {'question': question, 'show_question_info': True}
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': 'You did not select a choice',
+            },
+        )
+    else:
+        # selected_choice.votes += 1  # possible race conditions if 2 user are voting at the same time !!!
+        selected_choice.votes = F('votes') + 1  # avoid race condition by incrementing value in DB query
+        selected_choice.save()
+        selected_choice.refresh_from_db()  # reload obj to remove F(), in case save() called again in this view
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id, )))
